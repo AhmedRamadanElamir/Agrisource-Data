7303e51812961617957a4657ea83e20efced3d68
Ahmed Ramadan <ahmed.ramadan.e1994@gmail.com>
2016-07-19 07:30:30 +0200

    Update DHT11.c

--- DHT11.c
+++ DHT11.c
@@ -4,14 +4,14 @@ byte dat[5];
 byte read_data()
 {
     	byte data;
-    	for(int i=0; i8; i++)
+    	for(int i=0; i<8; i++)
     	{
     		if(digitalRead(DHpin) == LOW)
     		{
     			while(digitalRead(DHpin) == LOW);
     			delayMicroseconds(30);
     			if(digitalRead(DHpin) == HIGH)
-    				data = (1(7-i));
+    				data |= (1<<(7-i));
     			while(digitalRead(DHpin) == HIGH);
     		}
     	}
@@ -27,10 +27,10 @@ void start_test()
 	delayMicroseconds(40);
 	pinMode(DHpin,INPUT);
 	while(digitalRead(DHpin) == HIGH);
-	delayMicroseconds(80); 
+	delayMicroseconds(80); //
 	if(digitalRead(DHpin) == LOW);
 	delayMicroseconds(80);
-	for(int i=0;i4;i++)
+	for(int i=0;i<4;i++)
 		dat[i] = read_data();
 	pinMode(DHpin,OUTPUT);
 	digitalWrite(DHpin,HIGH);
@@ -47,12 +47,12 @@ void setup()
 void loop()
 {
 	start_test();
-	Serial.print(Current humdity = );
+	Serial.print("Current humdity = ");
 	Serial.print(dat[0], DEC);
 	Serial.print('.');
 	Serial.print(dat[1],DEC);
 	Serial.println('%');
-	Serial.print(Current temperature = );
+	Serial.print("Current temperature = ");
 	Serial.print(dat[2], DEC);
 	Serial.print('.');
 	Serial.print(dat[3],DEC);
