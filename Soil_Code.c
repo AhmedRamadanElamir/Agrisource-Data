41705ef7b400931b23b025c54cf2586cbbc1532e
SavageCoder <omaralaa14895@gmail.com>
2016-07-22 01:33:11 +0200

    Add files via upload

---
+++ Soil_Code.c
@@ -0,0 +1,14 @@
+int soil = PB_5;
+int soilData;
+void setup()
+{
+ Serial.begin(9600);
+ pinMode(soil,INPUT);
+}
+
+void loop()
+{
+  soilData = analogRead(soil));
+  Serial.println(soilData);
+  delay(1000);
+}
