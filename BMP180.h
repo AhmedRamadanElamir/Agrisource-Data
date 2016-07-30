b5472c2236f2a9cd1e43d8acc64fbf7761a5b879
SavageCoder <omaralaa14895@gmail.com>
2016-07-30 22:00:07 +0200

    Update BMP180.h

--- BMP180.h
+++ BMP180.h
@@ -1,9 +1,7 @@
 #ifndef BMP180_h
 #define BMP180_h
 
-char debug(char *fmt, ... );
-double calculate_sea_level(double p,double alt);
-void readSensor();
+void readSensor(int &temperature, int &pressure);
 void  getCalibrationData() ;
 
 #endif
