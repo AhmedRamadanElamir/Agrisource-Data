09a5806e6a74854143dbc107e3a90f6435aed41e
SavageCoder <omaralaa14895@gmail.com>
2016-07-30 21:59:05 +0200

    Update BMP180.c

--- BMP180.c
+++ BMP180.c
@@ -7,10 +7,6 @@ TwoWire i2c_port(2);
 int32_t temperature = 0;
 int32_t pressure = 0;
 
-double hpa_current=0;
-double hpa_baseline=0;
-double temperature_c=0;
-
 void setup()
 {
   Serial.begin(9600);
@@ -18,21 +14,15 @@ void setup()
   i2c_port.begin();
   delay(10);
   getCalibrationData();
-  
-  readSensor();
-  hpa_baseline=pressure*0.01;
 }
 
 void loop()
 {
-  readSensor();
-  hpa_current=pressure*0.01;
-  temperature_c=temperature/10.0;
-  
-  debug("Temperature:%.2f C\r\n",temperature,temperature_c); 
-  debug("Pressure:%.2f hPa\r\n",pressure,hpa_current);
-
-#define hpa_sea 1013.25
+  readSensor(temperature,pressure);
+  Serial.print("temp.= ");
+  Serial.print(temperature);
+  Serial.print(", pressure= ");
+  Serial.println(pressure);
   delay(1000);
 }
 
