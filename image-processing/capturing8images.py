e0c88d8b1024f683ab3398b03bdcab47531d7e2c
Omar_Alaa <omaralaa14895@gmail.com>
2016-08-10 03:08:46 +0200

    Update capturing8images.py

--- image-processing/capturing8images.py
+++ image-processing/capturing8images.py
@@ -1,33 +1,13 @@
+# cv2 package , python 2.x
+
+import time
 import cv2
- 
-# Camera 0 is the integrated web cam on my netbook
-camera_port = 0
- 
-#Number of frames to throw away while the camera adjusts to light levels
-ramp_frames = 30
- 
-# Now we can initialize the camera capture object with the cv2.VideoCapture class.
-# All it needs is the index to a camera port.
+ports = [1,2,3,4,5,6,7,8]; #specify port numbers
+for( i=0 ; i<8;i++){
+camera_port = ports[i]
 camera = cv2.VideoCapture(camera_port)
- 
-# Captures a single image from the camera and returns it in PIL format
-def get_image():
- # read is the easiest way to get a full image out of a VideoCapture object.
- retval, im = camera.read()
- return im
- 
-# Ramp the camera - these frames will be discarded and are only used to allow v4l2
-# to adjust light levels, if necessary
-for i in xrange(ramp_frames):
- temp = get_image()
-print("Taking image...")
-# Take the actual image we want to keep
-camera_capture = get_image()
-file = "/home/codeplasma/test_image.png"
-# A nice feature of the imwrite method is that it will automatically choose the
-# correct format based on the file extension you provide. Convenient!
-cv2.imwrite(file, camera_capture)
- 
-# You'll want to release the camera, otherwise you won't be able to create a new
-# capture object until your script exits
-del(camera)
+time.sleep(0.1)  # If you don't wait, the image will be dark
+return_value, image = camera.read()
+cv2.imwrite(""+i+".png", image) # specify the directory to be saved
+del(camera)  # so that others can use the camera as soon as possible
+}
