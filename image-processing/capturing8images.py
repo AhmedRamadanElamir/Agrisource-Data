e3b3b93d4205aa1bfd9a29ef1a1cd834d66aa0d5
Omar_Alaa <omaralaa14895@gmail.com>
2016-08-10 03:15:17 +0200

    Update capturing8images.py

--- image-processing/capturing8images.py
+++ image-processing/capturing8images.py
@@ -3,11 +3,11 @@
 import time
 import cv2
 ports = [1,2,3,4,5,6,7,8]; #specify port numbers
-for( i=0 ; i<8;i++){
-camera_port = ports[i]
-camera = cv2.VideoCapture(camera_port)
-time.sleep(0.1)  # If you don't wait, the image will be dark
-return_value, image = camera.read()
-cv2.imwrite(""+i+".png", image) # specify the directory to be saved
-del(camera)  # so that others can use the camera as soon as possible
-}
+for i in ports:
+  camera_port = i
+  camera = cv2.VideoCapture(camera_port)
+  time.sleep(0.1)  # If you don't wait, the image will be dark
+  return_value, image = camera.read()
+  cv2.imwrite("D:\ASD internship\"+i+".png", image) # specify the directory to be saved
+  del(camera)  # so that others can use the camera as soon as possible
+
